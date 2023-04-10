aa359645766eb245649f02179da98972cd24e838
loudsheep <loudsheep05@gmail.com>
2023-04-10 18:42:57 +0200

    Inintail commit and first test chart

---
+++ src/App.jsx
@@ -0,0 +1,16 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import LineChart from './Charts/LineChart'
+import './App.css'
+
+function App() {
+
+  return (
+    <div className="App">
+      <LineChart></LineChart>
+    </div>
+  )
+}
+
+export default App
