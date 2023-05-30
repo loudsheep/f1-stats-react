8ebd8947783c19b5a3ec6e2188efa6332fa9ea77
loudsheep <loudsheep05@gmail.com>
2023-05-30 21:05:26 +0200

    Mini sectors chart

--- src/components/Charts/DriverTelemetryComparison.jsx
+++ src/components/Charts/DriverTelemetryComparison.jsx
@@ -2,6 +2,7 @@ import { useState } from 'react';
 import myData from '../../assets/testData1.json';
 import myData2 from '../../assets/testData2.json';
 import LinearChart from './Telemetry/LinearChart';
+import MiniSectorsChart from './Telemetry/MiniSectorsChart';
 
 export default function DriverTelemetryComparison() {
     const data = [
@@ -9,13 +10,13 @@ export default function DriverTelemetryComparison() {
         { color: "#c40000", name: "LEC", data: myData2.speed },
     ];
 
-    const [title, setTitle] = useState("Speed data");
+    const trackData = [
+        { color: "#0000ff", name: "VER", data: myData.time },
+        { color: "#c40000", name: "LEC", data: myData2.time },
+    ];
 
     return <>
-        <LinearChart title={title} chartData={data}></LinearChart>
-
-        <button onClick={() => {
-            setTitle("Some random text" + Math.random()) 
-        }}>TEXT</button>
+        <LinearChart title={"Speed data VER - LEC"} chartData={data}></LinearChart>
+        <MiniSectorsChart title={"Mini sectors VER - LEC"} trackMap={myData.track_map} timeData={trackData}></MiniSectorsChart>
     </>
 }
---
+++ src/components/Charts/Telemetry/MiniSectorsChart.jsx
@@ -0,0 +1,131 @@
+import { useLayoutEffect } from "react";
+import * as am5 from "@amcharts/amcharts5";
+import * as am5xy from "@amcharts/amcharts5/xy";
+import * as am5plugins_exporting from "@amcharts/amcharts5/plugins/exporting";
+
+export default function MiniSectorsChart({ title, trackMap, timeData, miniSectorCount = 20 }) {
+    
+    const findDataMinAndMax = (data, key = "Y") => {
+        let max = Number.NEGATIVE_INFINITY;
+        let min = Number.POSITIVE_INFINITY;
+
+        for (let i of data) {
+            if (i.Y < min) {
+                min = i[key];
+            }
+            if (i.Y > max) {
+                max = i[key]
+            }
+        }
+
+        return {
+            min: min,
+            max: max,
+        };
+    };
+
+    const createChart = (id, track, timeData, miniSectorsCount) => {
+        let root = am5.Root.new(id);
+
+        let chart = root.container.children.push(
+            am5xy.XYChart.new(root, {
+                panY: false,
+                layout: root.verticalLayout,
+            })
+        );
+
+        let timeMinMax = findDataMinAndMax(timeData[0].data, "X");
+        let miniSectorDistance = timeMinMax.max / miniSectorsCount;
+
+        track = JSON.parse(JSON.stringify(track));
+
+        // let sectorTimes
+
+        let sectorColors = [];
+        for (let i = 0; i < miniSectorsCount + 1; i++) {
+            sectorColors.push({
+                color: "#000",
+                time: Number.POSITIVE_INFINITY,
+            })
+        }
+
+        for (let time of timeData) {
+            let sectorTmpTime = 0;
+            let currentSector = 0;
+            let sectors = [];
+            for (let i of time.data) {
+                let sec = Math.floor(i.X / miniSectorDistance);
+                if (sec != currentSector) {
+                    sectors.push(i.Y - sectorTmpTime);
+                    if (sectorColors[currentSector].time > i.Y - sectorTmpTime) {
+                        sectorColors[currentSector] = {
+                            color: time.color,
+                            time: i.Y - sectorTmpTime,
+                        }
+                    }
+                    sectorTmpTime = i.Y;
+                    currentSector = sec;
+                }
+            }
+        }
+
+        for (let i in track) {
+            let sec = Math.floor(timeData[0].data[i].X / miniSectorDistance);
+
+            track[i].strokeSettings = {
+                stroke: am5.color(sectorColors[sec].color),
+            }
+        }
+
+        // Create X-Axis
+        let xAxis = chart.xAxes.push(
+            am5xy.ValueAxis.new(root, {
+                renderer: am5xy.AxisRendererX.new(root, {}),
+                visible: false,
+            })
+        );
+
+        // Create Y-axis
+        let yAxis = chart.yAxes.push(
+            am5xy.ValueAxis.new(root, {
+                renderer: am5xy.AxisRendererY.new(root, {}),
+                syncWithAxis: xAxis,
+                visible: false,
+            })
+        );
+        // Create series
+        let series = chart.series.push(
+            am5xy.SmoothedXYLineSeries.new(root, {
+                // name: name,
+                xAxis: xAxis,
+                yAxis: yAxis,
+                valueYField: "Y",
+                valueXField: "X",
+                stroke: am5.color("#fff"),
+                fill: am5.color("#ff0000"),
+            })
+        );
+        series.data.setAll(track);
+
+        series.strokes.template.setAll({
+            strokeWidth: 6.5,
+            templateField: "strokeSettings",
+        });
+
+        return () => {
+            root.dispose();
+        };
+    };
+
+    useLayoutEffect(() => {
+        let chart = createChart("map-chart", trackMap, timeData, miniSectorCount);
+
+        return () => {
+            chart();
+        }
+    });
+
+    return <>
+        <div id="map-chart" className="chart" style={{ width: "50%", aspectRatio: 1, marginBottom: "50px" }}></div>
+    </>;
+}
